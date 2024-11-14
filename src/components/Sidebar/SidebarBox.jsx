6e04355a47cf84e7c1b6a4a5cc524f4f13432292
mobin <mobinizadi.dev@gmail.com>
2024-11-14 12:45:18 +0330

    Add isActive feature to sidebar links

--- src/components/Sidebar/SidebarBox.jsx
+++ src/components/Sidebar/SidebarBox.jsx
@@ -8,11 +8,10 @@ export default function SidebarBox({ title, items, className }) {
             <span className='font-bold text-sm text-gray-400'>{title}</span>
             <ul className='space-y-1 ml-3 mt-2'>
                 {items.map(item => (
-                    <li key={item.id} className='transition-all hover:bg-blue-50 rounded-lg px-1 py-2' >
-                        <NavLink to={item.path} className='flex items-center gap-1'>
+                    <li key={item.id} >
+                        <NavLink to={item.path} className={({ isActive }) => `flex items-center gap-1 transition-all hover:bg-blue-50 rounded-lg px-1 py-2 ${isActive ? 'bg-blue-50' : ''}`}>
                             {React.cloneElement(item.icon, { className: '!w-6 !h-6' })}
                             {item.title}
-
                         </NavLink>
                     </li>
 
